c1123cd088b889d0e86c51264fe5f8d161d46da6
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2018-05-09 23:27:58 +0200

    First working version of `Label` widget (with tests)
    Features:
    
    - `Widget` base class
    - text alignment
    - local, inherited (parent widget) and default attributes
    - true color (RGB 24 bits)
    - multiple units, including percentages
    - margin, border, outline and padding (independent for each side)
    - auto-adjust widgets width and height (both not defined or render ignored)

---
+++ __tests__/Label.js
@@ -0,0 +1,50 @@
+const {Label} = require('..')
+
+
+test('empty', function()
+{
+  const label = new Label()
+
+  expect(label.render()).toEqual([''])
+})
+
+test('simple text', function()
+{
+  const label = new Label({text: 'simple text'})
+
+  expect(label.render()).toEqual(['simple text'])
+})
+
+test('colored', function()
+{
+  const label = new Label({
+    backgroundColor: 'rgb(255,0,0)',
+    foregroundColor: 'rgb(0,255,0)',
+    text: 'colored'
+  })
+
+  expect(label.render())
+  .toEqual(['\u001b[38;2;0;255;0m\u001b[48;2;255;0;0mcolored\u001b[49m\u001b[39m'])
+})
+
+test('height', function()
+{
+  const label = new Label({height: 4, text: 'height'})
+
+  expect(label.render()).toEqual(['height', '      ', '      ', '      '])
+})
+
+test('width', function()
+{
+  const label = new Label({text: 'width', width: 10})
+
+  expect(label.render()).toEqual(['width     '])
+})
+
+test('height and width', function()
+{
+  const label = new Label({height: 4, text: 'asdf', width: 10})
+
+  expect(label.render())
+  .toEqual(['asdf      ', '          ', '          ', '          '])
+})
