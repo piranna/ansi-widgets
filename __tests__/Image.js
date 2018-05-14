af3f86f072b5a471294ba36436be39609ce33790
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2018-05-14 17:41:26 +0200

    `Image` widget

---
+++ __tests__/Image.js
@@ -0,0 +1,17 @@
+const {Image} = require('..')
+
+
+test('empty', async function()
+{
+  const image = new Image()
+
+  expect(await image.render()).toEqual([])
+})
+
+test('local image', async function()
+{
+  const image = new Image({url: `${__dirname}/fixtures/dog.jpg`})
+
+  // require('fs').writeFileSync(`${__dirname}/fixtures/dog.json`, JSON.stringify(await image.render(), null, 2))
+  expect(await image.render()).toEqual(require(`${__dirname}/fixtures/dog.json`))
+})
